f6902ba9fd002753586a92e61f5575ea43f39aed
Artur Karczmarczyk <artur@ideaspot.pl>
2023-11-13 16:00:47 +0100

    components, routing

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit{
+  personId?: number;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.personId = params['id'];
+    });
+  }
+}
