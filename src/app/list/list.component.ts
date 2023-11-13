6ded9bb153ee349e74f28c595cf2ea3ea6211864
Artur Karczmarczyk <artur@ideaspot.pl>
2023-11-13 16:38:41 +0100

    service, list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Person} from "../person";
+import {PersonLsService} from "../person-ls.service";
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  people: Person[] = [];
 
+  constructor(
+    private personLsService: PersonLsService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.people = this.personLsService.getAll();
+  }
 }
